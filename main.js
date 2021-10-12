bc1218e74ae3f8bf37e8e28e6416d9bb56809fcc
Carlos Gomez <carlosandres0741@gmail.com>
2021-10-11 23:17:20 -0500

    Finish counter

--- main.js
+++ main.js
@@ -1,6 +1,27 @@
 import './style.css'
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+const $counter = document.querySelector('#counter');
+let counter = 0;
+const $btn_increment = document.querySelector('#btn_increment');
+const $btn_decrement = document.querySelector('#btn_decrement');
+const $btn_reset = document.querySelector('#btn_reset');
+
+const increment = () => {
+  counter += 1;
+  $counter.textContent = counter;
+}
+
+const decrement = () => {
+  counter -= 1;
+  $counter.textContent = counter;
+}
+
+const reset = () => {
+  counter = 0;
+  $counter.textContent = counter;
+}
+
+$btn_increment.addEventListener('click', increment)
+$btn_decrement.addEventListener('click', decrement)
+$btn_reset.addEventListener('click', reset)
+
